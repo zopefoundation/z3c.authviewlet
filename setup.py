6c8604496194decf24735420fa693a6806840271
Michael Howitz <mh@gocept.com>
2018-10-18 20:24:37 +0200

    Preparing release 1.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup(
     name='z3c.authviewlet',
-    version='1.1.dev0',
+    version='1.1',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
     description="Authentication viewlet for Zope3",
